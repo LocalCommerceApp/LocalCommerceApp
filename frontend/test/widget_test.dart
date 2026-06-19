ebc3e74bf29fa2f653f41840be14fed3a1a66c18
Sayan Pandit <sayanpandit755@gmail.com>
2026-06-19 20:36:05 +0530

    chore: Rename app to Vytra in configurations

--- frontend/test/widget_test.dart
+++ frontend/test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:testapp/main.dart';
+import 'package:vytra/main.dart';
 
 void main() {
   testWidgets('Login screen displays correctly', (WidgetTester tester) async {
